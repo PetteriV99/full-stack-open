8715efa9d6f866ae0983a00b550520ebaed8299d
Petteri Vänttinen <petteri.vanttinen@edu.turkuamk.fi>
2023-06-14 01:31:50 +0300

    Task 1.6

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,5 +1,15 @@
 import { useState } from 'react'
 
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const Item = (props) => (
+  <p>{props.text} {props.value}</p>
+)
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
@@ -8,7 +18,18 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <h1>Give feedback</h1>
+      <div>
+        <Button handleClick={() => setGood(good + 1)} text="Good"></Button>
+        <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral"></Button>
+        <Button handleClick={() => setBad(bad + 1)} text="Bad"></Button>
+      </div>
+      <h1>Stats</h1>
+      <div>
+        <Item text="Good" value={good}></Item>
+        <Item text="Neutral" value={neutral}></Item>
+        <Item text="Bad" value={bad}></Item>
+      </div>
     </div>
   )
 }

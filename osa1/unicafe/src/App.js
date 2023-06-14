d75190de6238d133e623ab6383eb8aab0cb3d333
Petteri Vänttinen <petteri.vanttinen@edu.turkuamk.fi>
2023-06-14 13:30:22 +0300

    Task 1.8

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -10,16 +10,33 @@ const Item = (props) => (
   <p>{props.text} {props.value}</p>
 )
 
+const Statistics = (props) => {
 
+  let all = props.values.good + props.values.neutral + props.values.bad
+  let values = props.values.good + (props.values.bad * -1)
+
+  return (
+    <div>
+      {all !== 0 ?
+        <div>
+          <Item text="Good" value={props.values.good}></Item>
+          <Item text="Neutral" value={props.values.neutral}></Item>
+          <Item text="Bad" value={props.values.neutral}></Item>
+          <Item text='All' value={all}></Item>
+          <Item text='Average' value={values / all}></Item>
+          <Item text='Positive' value={props.values.good / all * 100 + ' %'}></Item>
+        </div>
+        : <p>No feedback given</p>
+      }
+    </div>
+  )
+}
 
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  let all = good + neutral + bad
-  let values = good + (bad * -1)
 
   return (
     <div>
@@ -30,16 +47,15 @@ const App = () => {
         <Button handleClick={() => setBad(bad + 1)} text="Bad"></Button>
       </div>
       <h1>Stats</h1>
-      <div>
-        <Item text="Good" value={good}></Item>
-        <Item text="Neutral" value={neutral}></Item>
-        <Item text="Bad" value={bad}></Item>
-        <Item text='All' value={all}></Item>
-        <Item text='Average' value={values / all}></Item>
-        <Item text='Positive' value={good / all * 100 + ' %'}></Item>
-      </div>
+      <Statistics values={{good, neutral, bad}}></Statistics>
     </div>
   )
 }
 
 export default App
+
+/*
+<Item text='All' value={all}></Item>
+<Item text='Average' value={values / all}></Item>
+<Item text='Positive' value={good / all * 100 + ' %'}></Item>
+*/

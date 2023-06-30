69c61e0c365494a98fa98775d4a49f97ef71ffba
Petteri Vänttinen <petteri.vanttinen@edu.turkuamk.fi>
2023-06-30 15:02:03 +0300

    Add task 2.11

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Persons = (params) => {
   return (
-    params.values.map(person => <p key={person.name}>{person.name} {person.number}</p>)
+    params.values.map(person => <p key={person.id}>{person.name} {person.number}</p>)
   )
 }
 
@@ -33,15 +34,22 @@ const PersonForm = (params) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterQuery, setFilterQuery] = useState('')
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -91,7 +99,7 @@ const App = () => {
         onChangeNumber={handleChangeNumber}
       />
       <h2>Numbers</h2>
-      <Persons values={filteredPersons}/>
+      <Persons values={filteredPersons} />
     </div>
   )
 

f3ec405e92b426d2f974e8c2ecbcec11fc28490b
Petteri Vänttinen <petteri.vanttinen@edu.turkuamk.fi>
2023-07-06 15:49:52 +0300

    Complete task 2.18

--- osa2/maat/src/App.js
+++ osa2/maat/src/App.js
@@ -2,20 +2,35 @@ import { useState, useEffect } from 'react'
 import countryService from './services/countries'
 
 const Countries = (params) => {
-  if (params.show) {
-  return (
-    <div>{params.values.map(country => 
-      <p key={country.name.common}>{country.name.common}</p>
-    )}
-    </div>
-  )}
-  else { return (<p>too many countries match, specify another filter</p>) }
+
+  const filteredCountries = params.values.filter(country => country.name.common.toLocaleLowerCase().includes(params.query.toLocaleLowerCase()))
+  if (filteredCountries.length < 10 && filteredCountries.length != 1) {
+    return (
+      <div>{filteredCountries.map(country =>
+        <p key={country.name.common}>{country.name.common}</p>
+      )}
+      </div>
+    )
+  }
+  else if (filteredCountries.length === 1) {
+    return (<CountryInfo values={filteredCountries[0]}></CountryInfo>)
+  }
+  else { return (<p>Too many countries, adjust filter</p>) }
 }
 
-const countryInfo = (params) => {
+const CountryInfo = (params) => {
   return (
     <div>
-      <h1>{params.name}</h1>
+      <h1>{params.values.name.common}</h1>
+      <p>capital {params.values.capital}</p>
+      <p>area {params.values.area}</p>
+      <h2>languages:</h2>
+      <ul>
+        {Object.entries(params.values.languages).map(([key, value]) => {
+          return(<li key={key}>{value}</li>)
+        })}
+      </ul>
+      <img src={params.values.flags.png}></img>
     </div>
   )
 }
@@ -32,7 +47,6 @@ const FilterForm = (params) => {
 
 const App = () => {
   const [countries, setCountries] = useState([])
-  const [showCountries, setShowCountries] = useState(false)
   const [filterQuery, setFilterQuery] = useState('')
 
   const hook = () => {
@@ -48,8 +62,6 @@ const App = () => {
 
   useEffect(hook, [])
 
-  const filteredCountries = countries.filter(country => country.name.common.toLocaleLowerCase().includes(filterQuery.toLocaleLowerCase()))
-
   const handleChangeQuery = (event) => {
     setFilterQuery(event.target.value)
   }
@@ -57,7 +69,7 @@ const App = () => {
   return (
     <div>
       <FilterForm value={filterQuery} onChange={handleChangeQuery} />
-      <Countries show={showCountries} values={filteredCountries}></Countries>
+      <Countries query={filterQuery} values={countries}></Countries>
     </div>
   )
 

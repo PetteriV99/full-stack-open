08ca096c359d72906c6b2a94078e5a5bb09bebdb
PetteriV99 <petterivanttinen99@gmail.com>
2023-07-06 16:44:14 +0300

    Add alt text and fix typo

--- osa2/maat/src/App.js
+++ osa2/maat/src/App.js
@@ -4,7 +4,7 @@ import countryService from './services/countries'
 const Countries = (params) => {
 
   const filteredCountries = params.values.filter(country => country.name.common.toLocaleLowerCase().includes(params.query.toLocaleLowerCase()))
-  if (filteredCountries.length < 10 && filteredCountries.length != 1) {
+  if (filteredCountries.length < 10 && filteredCountries.length !== 1) {
     return (
       <div>{filteredCountries.map(country =>
         <p key={country.name.common}>{country.name.common}</p>
@@ -30,7 +30,7 @@ const CountryInfo = (params) => {
           return(<li key={key}>{value}</li>)
         })}
       </ul>
-      <img src={params.values.flags.png}></img>
+      <img src={params.values.flags.png} alt={`Flag of ${params.values.name.common}`}></img>
     </div>
   )
 }

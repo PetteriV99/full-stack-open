702849344e300afa84818bce6cd876f8c6c1b12a
PetteriV99 <petterivanttinen99@gmail.com>
2023-07-25 15:36:50 +0300

    Add descriptions for tests for clarity

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -12,62 +12,70 @@ describe('when there is initial data for blogs', () => {
     await Blog.insertMany(data.listWithManyBlogs)
   })
 
-  test('blogs are returned as json', async () => {
-    await api
-      .get('/api/blogs')
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
-  })
 
-  test('there are as many blogs as in the initial list', async () => {
-    const response = await api.get('/api/blogs')
+  describe('blogs can be viewed', () => {
 
-    expect(response.body).toHaveLength(data.listWithManyBlogs.length)
-  })
+    test('blogs are returned as json', async () => {
+      await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    })
 
-  test('identifying property is named id', async () => {
-    const response = await api.get('/api/blogs')
+    test('there are as many blogs as in the initial list', async () => {
+      const response = await api.get('/api/blogs')
 
-    response.body.forEach((blog) => {
-      expect(blog.id).toBeDefined()
+      expect(response.body).toHaveLength(data.listWithManyBlogs.length)
+    })
+
+    test('identifying property is named id', async () => {
+      const response = await api.get('/api/blogs')
+
+      response.body.forEach((blog) => {
+        expect(blog.id).toBeDefined()
+      })
     })
   })
 
-  test('new blogs can be added with a POST request', async () => {
-    const newBlogObject = data.listWithOneBlog[0]
-    await api
-      .post('/api/blogs')
-      .send(newBlogObject)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
-    const response = await api.get('/api/blogs')
-    const contents = response.body.map(r => r.title)
+  describe('blogs can be created', () => {
 
-    expect(response.body).toHaveLength(data.listWithManyBlogs.length + 1)
-    expect(contents).toContain(data.listWithOneBlog[0].title)
-  })
+    test('new blogs can be added with a POST request', async () => {
+      const newBlogObject = data.listWithOneBlog[0]
+      await api
+        .post('/api/blogs')
+        .send(newBlogObject)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+      const response = await api.get('/api/blogs')
+      const contents = response.body.map(r => r.title)
 
-  test('new blog post without likes defined will be returned with likes property', async () => {
-    const newBlogObject = data.listWithOneBlog[0]
-    newBlogObject.likes = undefined
-    const result = await api
-      .post('/api/blogs')
-      .send(newBlogObject)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
+      expect(response.body).toHaveLength(data.listWithManyBlogs.length + 1)
+      expect(contents).toContain(data.listWithOneBlog[0].title)
+    })
 
-    expect(result.body.likes).toBeDefined()
-    expect(result.body.likes).toBe(0)
-  })
+    test('new blog post without likes defined will be returned with likes property', async () => {
+      const newBlogObject = data.listWithOneBlog[0]
+      newBlogObject.likes = undefined
+      const result = await api
+        .post('/api/blogs')
+        .send(newBlogObject)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
 
-  test('new blog post without title or url will not be created', async () => {
-    const newBlogObject = data.listWithOneBlog[0]
-    delete newBlogObject.title
-    delete newBlogObject.url
-    await api
-      .post('/api/blogs')
-      .send(newBlogObject)
-      .expect(400)
+      expect(result.body.likes).toBeDefined()
+      expect(result.body.likes).toBe(0)
+    })
+
+    test('new blog post without title or url will not be created', async () => {
+      const newBlogObject = data.listWithOneBlog[0]
+      delete newBlogObject.title
+      delete newBlogObject.url
+      await api
+        .post('/api/blogs')
+        .send(newBlogObject)
+        .expect(400)
+    })
+
   })
 
   describe('deletion of a blog', () => {
@@ -78,7 +86,7 @@ describe('when there is initial data for blogs', () => {
       await api.delete(`/api/blogs/${blogObject.id}`).expect(204)
       const blogsAtEnd = await data.blogInDb()
 
-      expect(blogsAtEnd).toHaveLength(data.listWithManyBlogs.length -  1)
+      expect(blogsAtEnd).toHaveLength(data.listWithManyBlogs.length - 1)
       const titles = blogsAtEnd.map(r => r.title)
       expect(titles).not.toContain(blogObject.title)
 

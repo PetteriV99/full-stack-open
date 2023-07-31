29b437d8b2c17189fd72e51db327954c1b9bfcf7
PetteriV99 <petterivanttinen99@gmail.com>
2023-07-31 16:02:00 +0300

    Task 4.23: Update to use user exractor

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -30,7 +30,7 @@ blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
-    user: user.id
+    user: user._id
   })
 
   const savedBlog = await blog.save()
@@ -39,7 +39,7 @@ blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
   response.status(201).json(savedBlog)
 })
 
-blogsRouter.delete('/:id', async (request, response) => {
+blogsRouter.delete('/:id', middleware.userExtractor, async (request, response) => {
 
   const user = request.user
   const blog = await Blog.findById(request.params.id)
@@ -56,18 +56,24 @@ blogsRouter.delete('/:id', async (request, response) => {
   }
 })
 
-blogsRouter.put('/:id', async (request, response) => {
+blogsRouter.put('/:id', middleware.userExtractor, async (request, response) => {
   const body = request.body
+  const user = request.user
 
-  const blog = {
+  const oldBlog = await Blog.findById(request.params.id)
+  const newBlog = {
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes
   }
-
-  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-  response.status(200).json(updatedBlog)
+  if ( oldBlog.user.toString() === user._id.toString() ) {
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, newBlog, { new: true })
+    response.status(200).json(updatedBlog)
+  }
+  else {
+    response.status(401).json({ error: 'blog not created by user' })
+  }
 })
 
 module.exports = blogsRouter

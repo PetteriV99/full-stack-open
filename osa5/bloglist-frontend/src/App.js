62129e62b69865a459115e473dcc5b9ccd5e3eff
PetteriV99 <petterivanttinen99@gmail.com>
2023-08-01 01:39:35 +0300

    Complete task 5.1

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -24,6 +24,7 @@ const App = () => {
       const user = await loginService.login({
         username, password
       })
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')

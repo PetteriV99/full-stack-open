109661cf8086149b98da96bfc93b635836ddeb09
PetteriV99 <petterivanttinen99@gmail.com>
2023-08-03 01:23:17 +0300

    revert

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -21,7 +21,7 @@ const App = () => {
     blogService.getAll().then(blogs =>
       setBlogs(blogs)
     )
-  }, [blogs])
+  }, [])
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')

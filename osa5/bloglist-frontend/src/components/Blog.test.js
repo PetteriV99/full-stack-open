541425d37680c81bb70f9656044951c92d55f0e9
PetteriV99 <petterivanttinen99@gmail.com>
2023-08-07 23:36:52 +0300

    Task 5.13

---
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, screen } from '@testing-library/react'
+import Blog from './Blog'
+
+test('renders content', () => {
+  const blog = {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+  }
+
+  render(<Blog blog={blog} />)
+
+  const element = screen.getByText('Component testing is done with react-testing-library')
+  expect(element).toBeDefined()
+})

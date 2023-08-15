3fcefa56893a3cb4a1e524662ff0345f03864fb4
PetteriV99 <petterivanttinen99@gmail.com>
2023-08-15 16:42:26 +0300

    Task 5.19

--- osa5/bloglist-frontend/cypress/e2e/bloglist.cy.js
+++ osa5/bloglist-frontend/cypress/e2e/bloglist.cy.js
@@ -1,5 +1,5 @@
-describe('Blog app', function() {
-  beforeEach(function() {
+describe('Blog app', function () {
+  beforeEach(function () {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
     const user = {
       name: 'Matti Luukkainen',
@@ -10,12 +10,12 @@ describe('Blog app', function() {
     cy.visit('http://localhost:3000')
   })
 
-  it('Login form is shown', function() {
+  it('Login form is shown', function () {
     cy.contains('login').click()
   })
 
-  describe('Login',function() {
-    it('succeeds with correct credentials', function() {
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
       cy.contains('login').click()
       cy.get('#username').type('mluukkai')
       cy.get('#password').type('salainen')
@@ -23,12 +23,25 @@ describe('Blog app', function() {
       cy.contains('Matti Luukkainen logged in')
     })
 
-    it('fails with wrong credentials', function() {
+    it('fails with wrong credentials', function () {
       cy.contains('login').click()
       cy.get('#username').type('mluukkai')
       cy.get('#password').type('awrwarwarwarrwa')
       cy.get('#login-button').click()
-      cy.contains('wrong credentials')
+      cy.get('.error').should('contain', 'wrong credentials')
+      cy.get('html').should('not.contain', 'Matti Luukkainen logged in')
     })
+  })
+
+  describe('when logged in', function () {
+    beforeEach(function () {
+      cy.login({ username: 'mluukkai', password: 'salainen' })
+    })
+
+    it('a new blog can be created', function () {
+      // ...
+    })
+
+    // ...
   })
 })

66ff333a0bc4fc365b63ee0e931ee677bd9dd263
PetteriV99 <petterivanttinen99@gmail.com>
2023-08-23 15:58:43 +0300

    Complete task 6.3

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -22,8 +22,29 @@ const initialState = anecdotesAtStart.map(asObject)
 const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
+  switch(action.type) {
+  case 'NEW_ANECDOTE':
+    return state.concat(action.payload)
+  case 'ADD_VOTE': {
+    const id = action.payload.id
+    const anecdoteToChange = state.find(n => n.id === id)
+    const changedAnecdote = {
+      ...anecdoteToChange,
+      votes: anecdoteToChange.votes + 1
+    }
+    console.log(id)
+    return state.map(anecdote => anecdote.id !== id ? anecdote : changedAnecdote)
+  }
+  default:
+    return state
+  }
+}
 
-  return state
+export const addVote = (id) => {
+  return {
+    type: 'ADD_VOTE',
+    payload: { id }
+  }
 }
 
 export default reducer

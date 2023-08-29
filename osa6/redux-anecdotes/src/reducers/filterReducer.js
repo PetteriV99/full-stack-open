a69ac8166881f860c63f70a3844b2e4b3c779caf
PetteriV99 <petterivanttinen99@gmail.com>
2023-08-29 17:35:35 +0300

    Complete task 6.10

--- osa6/redux-anecdotes/src/reducers/filterReducer.js
+++ osa6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,18 +1,18 @@
-const filterReducer = (state = 'ALL', action) => {
-  console.log('ACTION: ', action)
-  switch (action.type) {
-  case 'SET_FILTER':
-    return action.payload
-  default:
-    return state
-  }
-}
+import { createSlice } from '@reduxjs/toolkit'
 
-export const filterChange = filter => {
-  return {
-    type: 'SET_FILTER',
-    payload: filter,
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState: 'ALL',
+  reducers: {
+    filterChange(state, action) {
+      console.log(action)
+      if (action.payload === '') {
+        return state = 'ALL'
+      }
+      return action.payload
+    }
   }
-}
+})
 
-export default filterReducer
+export const { filterChange } = filterSlice.actions
+export default filterSlice.reducer

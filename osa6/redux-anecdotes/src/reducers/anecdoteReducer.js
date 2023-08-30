acbf0cc2d940953a0bd7febec926f5d72d68e8b8
PetteriV99 <petterivanttinen99@gmail.com>
2023-08-31 02:20:21 +0300

    Task 6.16

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import anecdoteService from '../services/anecdotes'
 
 const getId = () => (100000 * Math.random()).toFixed(0)
 
@@ -37,4 +38,12 @@ const anecdoteSlice = createSlice({
 })
 
 export const { createAnecdote, addVote, appendAnecdote, setAnecdotes } = anecdoteSlice.actions
+
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const notes = await anecdoteService.getAll()
+    dispatch(setAnecdotes(notes))
+  }
+}
+
 export default anecdoteSlice.reducer

6d0d8464a247b50d596591e582036b4ed8affb41
PetteriV99 <petterivanttinen99@gmail.com>
2023-09-25 21:27:17 +0300

    Task 7.11

---
+++ osa7/bloglist-frontend/src/reducers/blogReducer.js
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit'
+import blogService from '../services/blog'
+
+const blogSlice = createSlice({
+  name: 'blogs',
+  initialState: [],
+  reducers: {
+    appendBlog(state, action) {
+      state.push(action.payload)
+    },
+    setBlogs(state, action) {
+      return action.payload
+    },
+    updateBlog(state, action) {
+      let foundIndex = state.findIndex(blog => blog.id === action.payload.id)
+      state.splice(foundIndex, 1, action.payload)
+    }
+  }
+})
+
+export const { appendBlog, setBlogs, updateBlog } = blogSlice.actions
+
+export const initializeBlogs = () => {
+  return async dispatch => {
+    const blogs = await blogService.getAll()
+    dispatch(setBlogs(blogs))
+  }
+}
+
+export default blogSlice.reducer

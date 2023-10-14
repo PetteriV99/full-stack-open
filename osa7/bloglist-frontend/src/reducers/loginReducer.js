67467c56ea24108a4c2ebeb2f1a6037630106cac
PetteriV99 <petterivanttinen99@gmail.com>
2023-10-14 23:27:20 +0300

    Task 7.13

--- osa7/bloglist-frontend/src/reducers/loginReducer.js
+++ osa7/bloglist-frontend/src/reducers/loginReducer.js
@@ -19,7 +19,6 @@ export const { setLogin, removeLogin } = loginSlice.actions
 export const login = ({ username, password }) => {
   return async dispatch => {
     const user = await loginService.login(username, password)
-    console.log(user)
     dispatch(setLogin(user))
   }
 }

d343fca4f9c1ec80b72d360b183149ebac757f01
PetteriV99 <petterivanttinen99@gmail.com>
2023-10-19 01:45:31 +0300

    Task 7.14

--- osa7/bloglist-frontend/src/components/Users.js
+++ osa7/bloglist-frontend/src/components/Users.js
@@ -1,14 +1,23 @@
 import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { initializeUsers } from '../reducers/userReducer'
 
 const Users = () => {
 
+  const dispatch = useDispatch()
+
+  const users = useSelector(state => state.users)
+
   useEffect(() => {
-    null
+    dispatch(initializeUsers())
   }, [])
 
   return(
     <div>
       <h2>users</h2>
+      {users.map(user =>
+        <p key={user.name}>{user.name}</p>
+      )}
     </div>
   )
 

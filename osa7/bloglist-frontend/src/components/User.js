ff59c05916c31b4504ddffe666628cb8565484da
PetteriV99 <petterivanttinen99@gmail.com>
2023-11-01 03:59:32 +0200

    Task 7.20 Style individual user screen

--- osa7/bloglist-frontend/src/components/User.js
+++ osa7/bloglist-frontend/src/components/User.js
@@ -9,20 +9,28 @@ const User = ({ user }) => {
     )
   }
 
-  console.log(user)
-
-  return(
-    <div>
-      <h2>{user.name}</h2>
-      <h3>added blogs</h3>
-      { user.blogs.length > 0 ?
-        <ul>
-          {user.blogs.map(blog => <li key={blog.id}>{blog.title}</li>)}
-        </ul> : <p>no blogs</p>
-      }
+  return (
+    <div className="items-center justify-center px-8 pt-6">
+      <h2 className="items-center justify-center px-8 py-6 text-xl font-bold">User information</h2>
+      <div className="flex min-h-screen bg-gray-200">
+        <div className="w-full list-disc rounded bg-white px-8 pb-8 pt-6 shadow-md">
+          <div key={user.name} className=" py-4">
+            <div className="font-bold text-gray-700">
+              {user.name}
+            </div>
+            <h3 className="py-6 text-lg font-bold">added blogs</h3>
+            <div>
+              { user.blogs.length > 0 ?
+                <ul>
+                  {user.blogs.map(blog => <li key={blog.id}>{blog.title}</li>)}
+                </ul> : <p>no blogs</p>
+              }
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
-
 }
 
 export default User

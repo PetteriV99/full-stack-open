6341bacd6bab5b094589a62fe538ff7d9116240f
PetteriV99 <petterivanttinen99@gmail.com>
2023-11-28 23:53:41 +0200

    Complete task 8.8

--- osa8/library-frontend/src/index.js
+++ osa8/library-frontend/src/index.js
@@ -1,28 +1,15 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000',
   cache: new InMemoryCache(),
 })
 
-const query = gql`
-  query {
-    allAuthors  {
-      name,
-      born,
-      bookCount,
-      id
-    }
-  }
-`
-
-client.query({ query })
-  .then((response) => {
-    console.log(response.data)
-  })
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
+)

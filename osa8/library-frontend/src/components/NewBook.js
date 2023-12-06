7c2c590dd060aa9486b837f5b734d32671b0d05c
PetteriV99 <petterivanttinen99@gmail.com>
2023-12-06 23:57:35 +0200

    Task 8.10

--- osa8/library-frontend/src/components/NewBook.js
+++ osa8/library-frontend/src/components/NewBook.js
@@ -1,4 +1,16 @@
 import { useState } from 'react'
+import { gql, useMutation  } from '@apollo/client'
+
+const CREATE_BOOK = gql`
+  mutation createBook($title: String!, $author: String!, $published: String, $genres: [String]) {
+    addBook(
+      title: $title,
+      author: $author,
+      published: $published,
+      genres: $genres
+    )
+  }
+`
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('')

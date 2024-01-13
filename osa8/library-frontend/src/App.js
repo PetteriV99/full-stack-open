ad52ba3d8f6dfe7e6f489b24e9493fe73a304356
PetteriV99 <petterivanttinen99@gmail.com>
2024-01-13 23:55:20 +0200

    Task 8.18 basic login form

--- osa8/library-frontend/src/App.js
+++ osa8/library-frontend/src/App.js
@@ -1,6 +1,9 @@
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import Login from './components/Login';
+
+import React, { useState } from 'react';
 
 import {
   BrowserRouter as Router,
@@ -40,17 +43,18 @@ const ALL_BOOKS = gql`
 const App = () => {
   const authors = useQuery(ALL_AUTHORS)
   const books = useQuery(ALL_BOOKS)
+  const [token, setToken] = useState(null)
 
   if (authors.loading || books.loading)  {
     return <div>loading...</div>
   }
 
   if (!authors.data) {
-    return <div>no data</div>
+    return <div>no author data</div>
   }
-  console.log(books)
+
   if (!books.data) {
-    return <div>no data</div>
+    return <div>no books data</div>
   }
 
   return (
@@ -60,12 +64,14 @@ const App = () => {
         <Link to="/">authors</Link>
         <Link to="/books">books</Link>
         <Link to="/add">add</Link>
+        <Link to="/login">add</Link>
       </div>
 
       <Routes>
         <Route path="/" element={<Authors authors={authors.data.allAuthors}/>} />
         <Route path="/books" element={<Books books={books.data.allBooks}/>} />
         <Route path="/add" element={<NewBook />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
     </Router>

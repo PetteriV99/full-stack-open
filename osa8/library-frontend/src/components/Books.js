dd9b8e25549572fed0aee6d4ae37ad0988d6532a
PetteriV99 <petterivanttinen99@gmail.com>
2024-01-16 05:20:47 +0200

    Task 8.19

--- osa8/library-frontend/src/components/Books.js
+++ osa8/library-frontend/src/components/Books.js
@@ -1,15 +1,33 @@
+import React, { useState } from 'react'
+
 const Books = (props) => {
 
+  const [activeFilter, setActiveFilter] = useState('')
+  console.log(activeFilter)
+
   const books = props.books
-  console.log(books)
   if (!books) {
     return (<p>no data</p>)
   }
 
+  const genresArray = books.map(book => { return book.genres })
+  const genres = [].concat(...genresArray)
+  const filters = [...new Set(genres)]
+
+  const handleClick = (genre) => {
+    if (activeFilter) {
+      setActiveFilter('')
+    }
+    else {
+      setActiveFilter(genre)
+    }
+  }
+
+  const filteredBooks = activeFilter !== '' ? books.filter((book => { return book.genres.includes(activeFilter)})) : books
+
   return (
     <div>
       <h2>books</h2>
-
       <table>
         <tbody>
           <tr>
@@ -17,7 +35,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {filteredBooks.map((a) => (
             <tr key={a.id}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -26,6 +44,15 @@ const Books = (props) => {
           ))}
         </tbody>
       </table>
+      {filters.map((genre, index) => (
+          <button 
+          style={{
+            backgroundColor: activeFilter === genre ? '#007BFF' : 'transparent',
+            color: activeFilter === genre ? '#fff' : '#007BFF',
+            border: `2px solid ${activeFilter === genre ? '#007BFF' : '#ccc'}`,
+          }}
+          key={index} onClick={() => handleClick(genre)}>{genre}</button>
+        ))}
     </div>
   )
 }

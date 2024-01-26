df335b560cdb096aff0b3ad5b11d3265b8434167
PetteriV99 <petterivanttinen99@gmail.com>
2024-01-26 23:57:59 +0200

    Task 8.23

--- osa8/grapql-server/index.js
+++ osa8/grapql-server/index.js
@@ -8,6 +8,8 @@ const http = require('http')
 
 const { v1: uuid } = require('uuid')
 const jwt = require('jsonwebtoken')
+const { WebSocketServer } = require('ws')
+const { useServer } = require('graphql-ws/lib/use/ws')
 
 const typeDefs = require('./typedefs')
 const resolvers = require('./resolvers')
@@ -39,9 +41,28 @@ const server = new ApolloServer({
 const start = async () => {
   const app = express()
   const httpServer = http.createServer(app)
+
+  const wsServer = new WebSocketServer({    
+    server: httpServer,    
+    path: '/',  
+  })
+
+  const schema = makeExecutableSchema({ typeDefs, resolvers })
+  const serverCleanup = useServer({ schema }, wsServer)
+
   const server = new ApolloServer({    
     schema: makeExecutableSchema({ typeDefs, resolvers }),    
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],  
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer }),
+    {        
+      async serverWillStart() {          
+        return {            
+          async drainServer() {              
+            await serverCleanup.dispose();            
+            },          
+          };        
+        },      
+      },
+    ],  
   })
   await server.start()
   app.use(    
